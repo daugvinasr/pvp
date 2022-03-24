ba2aa4ccbb2ed8a991a177a8f7c58f2db473711c
Martynas09 <martynas200012@gmail.com>
2022-03-24 13:38:43 +0200

    Puslapio apačia

--- resources/views/fixers.blade.php
+++ resources/views/fixers.blade.php
@@ -3,10 +3,7 @@
     <div class="mb-16">
         <!-- Code block starts -->
         <dh-component>
-            <div class="container flex justify-center mx-auto pt-16">
-
-            </div>
-            <div class="w-full bg-gray-100 px-10 pt-10">
+            <div class="w-full px-10 pt-20">
                 <div class="container mx-auto">
                     <div role="list" aria-label="Behind the scenes People " class="lg:flex md:flex sm:flex items-center xl:justify-between flex-wrap md:justify-around sm:justify-around lg:justify-around">
                         <div role="listitem" class="xl:w-1/3 sm:w-3/4 md:w-2/5 relative mt-16 mb-32 sm:mb-24 xl:max-w-sm lg:w-2/5">
