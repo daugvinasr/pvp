82e8a086a144cc91d53ce45ffb4ffda7e5666f22
Martynas09 <79332444+Martynas09@users.noreply.github.com>
2022-04-12 18:22:59 +0300

    Įrenginių peržiūra

--- resources/views/devices.blade.php
+++ resources/views/devices.blade.php
@@ -2,16 +2,15 @@
 @section('content')
     <div class="flex items-center justify-center">
         <div class="grid grid-cols-6 gap-2 px-16 ">
-        @foreach($devices as $device)
+            @foreach($devices as $device)
                 <div class="flex flex-col gap-1 ">
-        <!-- Card -->
-        <a class="delay-50 duration-100 bg-white hover:bg-gray-100 p-5 rounded-lg w-60 group shadow-md" href="/guides/{{$device->devices_id}}">
-            <!-- Image Cover -->
-            <img src="https://picsum.photos/250/250" class="w-full rounded shadow" />
-
-            <!-- Title -->
-            <h3 class="text-black font-bold mt-5">{{$device->name}}</h3>
-        </a>
+                    <!-- Card -->
+                    <a class="delay-50 duration-100 bg-white hover:bg-gray-100 p-5 rounded-lg group shadow-md" href="/guides/{{$device->devices_id}}">
+                        <!-- Image Cover -->
+                        <img class="rounded shadow w-[140px] h-[140px]" src="{{$device->photo_url}}"  />
+                        <!-- Title -->
+                        <h3 class="text-black font-bold mt-2">{{$device->name}}</h3>
+                    </a>
                 </div>
             @endforeach
         </div>
